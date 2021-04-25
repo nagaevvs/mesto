5cf8e2c3a3c011f414de1b0213be9997817078bd
Vladimir Nagaev <v.s.nagaev@yandex.ru>
2021-04-25 12:59:25 +0300

    fixed font link

--- js/script.js
+++ js/script.js
@@ -18,3 +18,25 @@ function closePopUp() {
 
     popUp.classList.remove('popup_opened');
 }
+
+
+let formElement = document.querySelector('.popup__form')
+let nameInput = formElement.querySelector('[name="name"]')
+let jobInput = formElement.querySelector('[name="job"]')
+let profileInfo = document.querySelector('.profile__info')
+let profileName = profileInfo.querySelector('.profile__name')
+let profileJob = profileInfo.querySelector('.profile__about')
+
+nameInput.setAttribute('value', `${profileName.textContent}`);
+jobInput.setAttribute('value', `${profileJob.textContent}`);
+
+function formSubmitHandler(evt) {
+    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+    let profileName = profileInfo.querySelector('.profile__name')
+    let profileJob = profileInfo.querySelector('.profile__about')
+    profileName.textContent = nameInput.value
+    profileJob.textContent = jobInput.value
+    closePopUp()
+}
+
+formElement.addEventListener('submit', formSubmitHandler);
