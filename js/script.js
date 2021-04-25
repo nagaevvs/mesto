bf123784288460bcfd9c9acb8e98d67b1c4bce21
Vladimir Nagaev <v.s.nagaev@yandex.ru>
2021-04-25 11:39:00 +0300

    validation check

--- js/script.js
+++ js/script.js
@@ -1,25 +1,20 @@
 let popUp = document.querySelector('.popup')
-console.log(popUp.classList);
+
 
 let editButton = document.querySelector('.profile__edit-button');
 
-
 editButton.addEventListener('click', openPopUp);
 
-
 function openPopUp() {
-    console.log('работает')
+
     popUp.classList.add('popup_opened');
 }
-
-
 
 let buttonClose = document.querySelector('.popup__button-close');
 
 buttonClose.addEventListener('click', closePopUp);
 
-
 function closePopUp() {
-    console.log('работает')
+
     popUp.classList.remove('popup_opened');
 }
